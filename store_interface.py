fe677525bf03bd683ea5e4c76b6bfc62ada4ce87
Delaney H <11yomang11@gmail.com>
2022-04-04 11:35:55 -0700

    - creation of gui box

--- store_interface.py
+++ store_interface.py
@@ -10,3 +10,14 @@ from tkinter import ttk
 #  -add or remove product from cart
 #  -invoice creation on sale
 #  -area to input customer data (for new customer)
+
+# create the root window
+root = tk.Tk()
+root.title("Generic Vehicle Merchant Software")
+root.geometry("600x400")
+
+# create the main frame
+frame_home = ttk.Frame(root)
+frame_home.pack(fill=tk.BOTH, expand=True)
+
+root.mainloop()
