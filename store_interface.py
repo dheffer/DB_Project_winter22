1bf33dc8be86b863a70d81ecedeab1dce19dfd0a
Delaney H <11yomang11@gmail.com>
2022-04-04 10:53:44 -0700

    - added import.tk

--- store_interface.py
+++ store_interface.py
@@ -2,9 +2,11 @@
 This file is for creation of what the user will see
 Our version of our own DBMS
 """
+import tkinter as tk
+from tkinter import ttk
 
-#TODO: -area to enter username and password,
-# -area to place an order from a vendor
-# -add or remove product from cart
-# -invoice creation on sale
-# -area to input customer data (for new customer)
+# TODO: -area to enter username and password,
+#  -area to place an order from a vendor
+#  -add or remove product from cart
+#  -invoice creation on sale
+#  -area to input customer data (for new customer)
