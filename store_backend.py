2e90295769ca83d2aab6452b2cfd1da862721dbd
Delaney H <11yomang11@gmail.com>
2022-04-04 10:27:38 -0700

    -added TODO lists to store_backend.py and store_interface.py -changed db creation file to sql file

--- store_backend.py
+++ store_backend.py
@@ -2,3 +2,9 @@
 This file is for all of our functions and classes
 All of the math etc done on the backend
 """
+
+#TODO: -enter user_details(username n password),
+# -order parts or products from the vendor
+# -add or remove product from cart
+# -invoice creation
+# -input customer data (for new customer)
--- store_interface.py
+++ store_interface.py
@@ -2,3 +2,9 @@
 This file is for creation of what the user will see
 Our version of our own DBMS
 """
+
+#TODO: -area to enter username and password,
+# -area to place an order from a vendor
+# -add or remove product from cart
+# -invoice creation on sale
+# -area to input customer data (for new customer)
