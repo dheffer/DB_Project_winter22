8826079472f5092a2d4f48b9467966256c0a9073
BrandonJohnson97 <98834483+BrandonJohnson97@users.noreply.github.com>
2022-04-14 02:09:29 -0230

    Shopping cart g UI (#23)
    * -updated GUI layout
    *shifted vendor_update from back to frontend
    
    * Update store_backend.py
    
    * -updated order_from_vendor query
    -creating of ShoppingCart within GUI
    
    * -ShoppingCart partially functional
    
    * Finished
    
    Co-authored-by: Delaney H <11yomang11@gmail.com>

--- details.py
+++ details.py
@@ -2,15 +2,9 @@
 Login Details for Database
 """
 
+from mysql.connector import connect
+
 HOST = "localhost"
 USER = "root"
-PASS = "pass"  # DON'T SAVE YOUR PASSWORD TO GIT, MAKE SURE YOU REMOVE IT BEFORE PUSHING
+PASS = "password"  # DON'T SAVE YOUR PASSWORD TO GIT, MAKE SURE YOU REMOVE IT BEFORE PUSHING
 DATABASE = "generic_vehicle_merchant"
-
-
-# TODO: create a finalize order
-#   -also create invoice when order is finalized
-#   -will need to reference ShoppingCart class to remove items from database
-
-
-
